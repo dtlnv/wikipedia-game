a2b595cbf4eaae7a1374388d3d9982cfd08f640c
Dmitri T <dtelnov@virtru.com>
2023-08-06 14:09:46 +0300

    Add crx

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
